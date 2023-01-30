a73aacfad0a28aa2d6d953569a725984f76c9c1b
Eric Castro <castro.eric@hotmail.com>
2023-01-30 18:29:50 -0300

    application creation

---
+++ src/main/java/com/example/bhubchallengeapi/BhubChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bhubchallengeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhubChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhubChallengeApiApplication.class, args);
+	}
+
+}
